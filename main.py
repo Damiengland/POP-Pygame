1f7b1d605fdd64d4dea85c68ffc78ebb5e8a3171
Damien <damien.england@outlook.com>
2022-04-19 20:36:37 +1000

    Update UI Design

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from levels import Levels
 # Initialize pygame font library
 pygame.font.init()
 
+CUSTOM_FONT = pygame.font.Font("./assets/fonts/Modak-Regular.ttf", 48)
 
 # Fonts
 LIVES_FONT = pygame.font.SysFont('arialblack', 32)
@@ -26,7 +27,8 @@ FPS = 60
 
 
 # Load Images
-border_img = pygame.image.load("./assets/images/UI-border.png")
+frame_img = pygame.image.load("./assets/images/frame.png")
+bg_img = pygame.image.load("./assets/images/BG.png")
 start_img, start_img_hover = pygame.image.load("./assets/images/start-screen.png"), \
                              pygame.image.load("./assets/images/start-screen-hover.png")
 
@@ -42,7 +44,9 @@ levels = Levels(WIN)
 
 
 # Draw window
-def draw_window(lives, level, start_game):
+def draw_window(lives, level, start_game, time_left):
+
+    WIN.blit(bg_img, (0, 0))
 
     for spell in spells.spells:
         WIN.blit(spells.spell_img, spell)
@@ -50,17 +54,20 @@ def draw_window(lives, level, start_game):
     WIN.blit(wizard.current_image, (wizard.x_pos, wizard.y_pos))
 
     if start_game:
+
         levels.orbs.draw_orbs()
 
-        WIN.blit(border_img, (0, 0))
+        pygame.draw.rect(WIN, (160, 80, 245), pygame.Rect(100, 450, 700 * time_left, 50))
 
-        lives_text = LIVES_FONT.render(f"{str(lives)}", True, WHITE)
-        level_text = LIVES_FONT.render(f"{str(level)}", True, WHITE)
+        WIN.blit(frame_img, (0, 0))
+
+        lives_text = CUSTOM_FONT.render(f"{str(lives)}", True, WHITE)
+        level_text = CUSTOM_FONT.render(f"{str(level)}", True, WHITE)
         WIN.blit(level_text, (37, 75))
         WIN.blit(lives_text, (842, 75))
     else:
         mouse = pygame.mouse.get_pos()
-        if 360 < mouse[0] < 560 and 330 < mouse[1] < 400:
+        if 370 < mouse[0] < 530 and 400 < mouse[1] < 460:
             WIN.blit(start_img, (0, 0))
         else:
             WIN.blit(start_img_hover, (0, 0))
@@ -89,7 +96,7 @@ def timer_bar(current_time, level_time):
     current = current_time
     elapsed_time = current_time - level_time
     time_left = elapsed_time / 30000
-    pygame.draw.rect(WIN, (56, 56, 54), pygame.Rect(100, 450, 700 * time_left, 50))
+    return time_left
 
 
 # Main game function
@@ -112,8 +119,10 @@ def main():
         current_time = pygame.time.get_ticks()
 
         WIN.fill((249, 254, 255))
-        timer_bar(current_time, level_time)
-        draw_window(lives, current_level, start_game)
+
+        time_left = timer_bar(current_time, level_time)
+
+        draw_window(lives, current_level, start_game, time_left)
 
         if start_game:
             # Get keys user is currently pressing
@@ -153,7 +162,6 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-
             if event.type == pygame.MOUSEBUTTONUP:
                 level_time = pygame.time.get_ticks()
                 start_level(current_level)
